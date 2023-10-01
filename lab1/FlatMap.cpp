d502fd365ec7a2d31461bba2f50ea624d42b78cf
dadashasha <143979250+dadashasha@users.noreply.github.com>
2023-10-02 05:01:25 +0700

    Update FlatMap.cpp

--- lab1/FlatMap.cpp
+++ lab1/FlatMap.cpp
@@ -1,5 +1,28 @@
 #include "FlatMap.h"
 
+std::size_t FlatMap::binarySearch(const std::string& key) {
+    if (size == 0)
+        return 0;
+    object* lower_bound =
+        std::lower_bound(array, array + size, key, [](const object& , const std::string& k) { return kv.key < k; });
+    return lower_bound - array;
+}
+
+void FlatMap::extension(std::size_t newСapacity) {
+    if (newСapacity <= capacity) {
+        return ;
+    }
+
+    FlatMap temp;
+    temp.array = new object[newСapacity];
+    temp.capacity = newСapacity;
+    temp.size = size;
+    
+    std::copy(array, array + size, temp.array);
+    std::swap(array, temp.array);
+    std::swap(size, temp.size);
+    std::swap(capacity, temp.capacity);
+}
 
 //Стандартный коснтруктор
 FlatMap::FlatMap() : array(nullptr), size(0), capacity_(0) {}
@@ -22,5 +45,17 @@ void FlatMap::clear() {
 //Получить количество элементов в таблице
 std::size_t FlatMap::size() const {
     return size;
+}
+
+//Конструктор копирования
+FlatMap::FlatMap(const FlatMap& other_map) : array(nullptr), size(0), capacity(0) {
+    FlatMap temp;
+    temp.extension(other_map.capacity);
+    temp.size = other_map.size;
+    
+    std::copy(other_map.data, other_map.data + other_map.size, temp.data);
+    std::swap(data, temp.data);
+    std::swap(size, temp.size);
+    std::swap(capacity, temp.capacity);
 }
 

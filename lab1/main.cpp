f03c157b0e6179dfb716a6e686ee04bc997393bf
dadashasha <143979250+dadashasha@users.noreply.github.com>
2023-10-02 05:12:03 +0700

    Update main.cpp

--- lab1/main.cpp
+++ lab1/main.cpp
@@ -1,12 +1,8 @@
-
-#include "task1.h"
-#include "task2.h"
+#include "FlatMap.h"
 #include <iostream>
 
 int main() {
-    std::cout << task::say_hello("world") << "\n";
-    const int x = 2;
-    const int y = 3;
-    std::cout << x << " + " << y << " = " << task::plus(x, y) << "\n";
+    FlatMap t_00_;
+    
     return 0;
 }

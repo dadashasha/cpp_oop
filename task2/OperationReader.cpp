0d37962793492cd6aead1a82d218fd9d8ee2e872
dadashasha <143979250+dadashasha@users.noreply.github.com>
2023-11-13 01:09:36 +0700

    Update OperationReader.cpp
    -добавлена проверка содержимого файла конфигурации

--- task2/OperationReader.cpp
+++ task2/OperationReader.cpp
@@ -1,7 +1,7 @@
+#include <fstream>
 #include <sstream>
 #include <iostream>
-#include <vector>
-#include <string>
+#include <algorithm>
 #include "OperationReader.h"
 
 std::vector<Operation> OperationReader::createOperationList(std::ifstream& configFile) {
@@ -23,6 +23,11 @@ std::vector<Operation> OperationReader::createOperationList(std::ifstream& confi
         }
 
         Operation operation = {};
+
+        if (parts[0] != "mute" && parts[0] != "mix") {
+            std::cout << "Error: Ignoring incorrect configuration file contents." << std::endl;
+            continue;
+        }
         operation.action = parts[0];
         operation.outputFileName = parts[1];
         operation.inputFileName = parts[2];
@@ -32,10 +37,23 @@ std::vector<Operation> OperationReader::createOperationList(std::ifstream& confi
                 std::cerr << "Error: Insufficient arguments for mute operation." << std::endl;
                 continue;
             }
-            operation.startSec = std::stoi(parts[3]);
-            operation.endSec = std::stoi(parts[4]);
 
-        } else if (operation.action == "mix") {
+            if (!parts[3].empty() && std::all_of(parts[3].begin(), parts[3].end(), ::isdigit)) {
+                operation.startSec = std::stoi(parts[3]);
+            }
+            else {
+                std::cerr << "Error: Invalid startSec value." << std::endl;
+                continue;
+            }
+            if (!parts[4].empty() && std::all_of(parts[4].begin(), parts[4].end(), ::isdigit)) {
+                operation.endSec = std::stoi(parts[4]);
+            }
+            else {
+                std::cerr << "Error: Invalid endSec value." << std::endl;
+                continue;
+            }
+        }
+        else if (operation.action == "mix") {
             if (parts.size() < 5) {
                 std::cerr << "Error: Insufficient arguments for mix operation." << std::endl;
                 continue;
@@ -43,7 +61,6 @@ std::vector<Operation> OperationReader::createOperationList(std::ifstream& confi
             operation.inputFileName2 = parts[3];
             operation.startSec = std::stoi(parts[4]);
         }
-
         operations.push_back(operation);
     }
     return operations;

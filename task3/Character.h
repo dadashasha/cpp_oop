3ff4699d5664386db6f19f0effee7a9c48f53913
dadashasha <143979250+dadashasha@users.noreply.github.com>
2023-11-20 03:55:24 +0700

    Update Character.h

--- task3/Character.h
+++ task3/Character.h
@@ -1,13 +1,16 @@
 #pragma once
+#include <vector>
 
-class Character {
+class Character{
 private:
 	int x;
 	int y;
 public:
 	Character(int startX, int startY) : x(startX), y(startY) {}
 
-	void Move(int dx, int dy);
+
+	bool CanMove(int dx, int dy, const std::vector<std::vector<char>>& maze) const;
+	void Move(char key, const std::vector<std::vector<char>>& maze);
 
 	int GetX() const;
 	int GetY() const;

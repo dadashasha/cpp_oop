b81953493a640880c20aecf96cb1081d080599a7
dadashasha <143979250+dadashasha@users.noreply.github.com>
2023-11-20 03:55:07 +0700

    Update Character.cpp
    Передвижение и проверка на возможность передвижения

--- task3/Character.cpp
+++ task3/Character.cpp
@@ -1,8 +1,36 @@
 #include "Character.h"
 
-void Character::Move(int dx, int dy) {
-	x += dx;
-	y += dy;
+bool Character::CanMove(int dx, int dy, const std::vector<std::vector<char>>& maze) const{
+	int newX = (x + dx + maze[0].size()) % maze[0].size();
+	int newY = y + dy;
+
+	if (newX < 0 || newX >= maze[0].size() || newY < 0 || newY >= maze.size()) {
+		return false;
+	}
+
+	return (maze[newY][newX] != '#' && maze[newY][newX] != '_');
+}
+
+void Character::Move(char key, const std::vector<std::vector<char>>& maze) {
+	int dx = 0, dy = 0;
+
+	if (key == 'a') {
+		dx = -1;
+	}
+	if (key == 'd') {
+		dx = 1;
+	}
+	if (key == 'w') {
+		dy = -1;
+	}
+	if (key == 's') {
+		dy = 1;
+	}
+
+	if (CanMove(dx, dy, maze)) {
+		x = (x + dx + maze[0].size()) % maze[0].size();
+		y += dy;
+	}
 }
 int Character::GetX() const{
 	return x;

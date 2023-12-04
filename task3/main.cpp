f4cf74222974ef4192ecf7f29b7f0140e9d3fcdb
dadashasha <143979250+dadashasha@users.noreply.github.com>
2023-12-04 08:39:15 +0700

    Update main.cpp

--- task3/main.cpp
+++ task3/main.cpp
@@ -1,13 +1,11 @@
-#include "Character.h"
 #include "Game.h"
 
 int main(void) {
 	Game game;
-	char key = '\0';
 	while (true) {
 		game.Draw();
-		game.HandleInput(key);
-		Sleep(100);
+		game.HandleInput();
+		Sleep(400);
 	}
 	return 0;
 }

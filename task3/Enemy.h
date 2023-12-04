114270f70d59dc7bf1b81d841fe44bbb955611ce
dadashasha <143979250+dadashasha@users.noreply.github.com>
2023-12-04 08:44:55 +0700

    Update Enemy.h

--- task3/Enemy.h
+++ task3/Enemy.h
@@ -3,21 +3,17 @@
 #include <chrono>
 #include <cstdlib>
 #include <ctime>
-#include <vector>
+#include "Entity.h"
 
-class Enemy {
+class Enemy : public Entity {
 private:
-	int x;
-	int y;
-	int direction;
+	int direction;//направление движения врага
 
 public:
-	Enemy(int startX, int startY) : x(startX), y(startY) {
+	Enemy(int startX, int startY) : Entity(startX, startY) {
 		std::srand(static_cast<unsigned int>(std::time(nullptr)));
 		direction = std::rand() % 4;
 	}
 
-	void MoveRandom(std::vector<std::vector<char>>& maze);
-	int GetX() const;
-	int GetY() const;
+	void Move(std::vector<std::vector<char>>& maze);
 };

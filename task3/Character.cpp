cbda9d23494f4e293dccd5675c3e40caa1352665
dadashasha <143979250+dadashasha@users.noreply.github.com>
2023-12-04 08:45:39 +0700

    Update Character.cpp

--- task3/Character.cpp
+++ task3/Character.cpp
@@ -1,18 +1,8 @@
 #include "Character.h"
 
-bool Character::CanMove(int dx, int dy, const std::vector<std::vector<char>>& maze) const{
-	int newX = (x + dx + maze[0].size()) % maze[0].size();
-	int newY = y + dy;
-
-	if (newX < 0 || newX >= maze[0].size() || newY < 0 || newY >= maze.size()) {
-		return false;
-	}
-
-	return (maze[newY][newX] != '#' && maze[newY][newX] != '_');
-}
-
-void Character::Move(char key, const std::vector<std::vector<char>>& maze) {
-	int dx = 0, dy = 0;
+void Character::Move(std::vector<std::vector<char>>& maze, char key) {
+	int dx = 0;
+	int dy = 0;
 
 	if (key == 'a') {
 		dx = -1;
@@ -27,14 +17,11 @@ void Character::Move(char key, const std::vector<std::vector<char>>& maze) {
 		dy = 1;
 	}
 
-	if (CanMove(dx, dy, maze)) {
-		x = (x + dx + maze[0].size()) % maze[0].size();
-		y += dy;
+	int newX = GetX() + dx;
+	int newY = GetY() + dy;
+
+	if (CanMove(newX, newY, maze)) {
+		x = newX;
+		y = newY;
 	}
-}
-int Character::GetX() const{
-	return x;
-}
-int Character::GetY() const {
-	return y;
 }
